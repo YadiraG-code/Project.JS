3ef5c7920314af92b5249d914248e883480dfc32
YadiraG-code <yadiragalvan58@gmail.com>
2026-06-26 11:28:44 -0700

    js.add

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // API 1: "http://www.omdbapi.com/?i=tt3896198&apikey=ed322d71"
+// API 2: "http://www.omdbapi.com/apikey.aspx?VERIFYKEY=2820092f-1154-4477-b6a8-4f9a3879f570"
+const movieListEL = document.querySelector(".movie-list");
+
+async function main() {
+    const movie = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ed322d71");
+    const movieData = await movie.json();
+    movieListEL.innerHTML + movieData.map((movie) => moviesHTML(movie)).join("");
+}
+
+main();
+
+function showMovieTitle(id) {
+    localStorage.setItem("id", id);
+    window.location.href = `${window.location.origin}/movie.html`
+}
+
+function movieHTML(movie) {
+   return ` <div class="movie">
+    <figure class="movie__img--wrapper">
+      <img class="movie__img" src="https://${movie.img}" alt="">
+      </figure>
+      <div>${movie.title}<b>
+        Guardians of the Galaxy: Vol.2</b>
+      </div>
+      <div>${movie.ratings}
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+          </div>
+          </div>`;
+}
--- movie.js
+++ movie.js
@@ -1,28 +1,20 @@
 const movieListEL = document.querySelector(' .movie-list');
-const id = localStorage.getItem("id")
+const movie = localStorage.getItem("id")
 
 async function onSearchChange(event) {
-    const id = event.target.value;
+    const movie = event.target.value;
     renderMovies(id)
 }
 
 async function renderMovies(id) {
-    const movie = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=ed322d71&s=${id}`)
+    const movie = await fetch(`http://www.omdbapi.com/apikey.aspx?VERIFYKEY=2820092f-1154-4477-b6a8-4f9a3879f570=${movie}`)
     const movieData = await movie.omd();
     movieListEL.innerHTML = movieData.map(movie => moviesHTML(movie)).join('');
 }
 
 function moviesHTML(movie) {
    return `
-    <div class="movie">
-    <div class="movie__title">
-    ${movie.title} 
-    </div>
-    <p Class="movie__body">
-    ${movie.body}
-    </p>
-    </div>
     `
 }
 
-renderMovies(id);
+renderMovies(movie);
