ac201240e7d108867c3a2a96a9e3c6039d6afd63
YadiraG-code <yadiragalvan58@gmail.com>
2026-07-22 16:45:06 -0700

    js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // API 1: "http://www.omdbapi.com/?i=tt3896198&apikey=ed322d71"
 // API 2: "http://www.omdbapi.com/apikey.aspx?VERIFYKEY=2820092f-1154-4477-b6a8-4f9a3879f570";
 
-fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ed322d71`)
-.then(response => response.jason())
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=ed322d71&s=SEARCH_TERM')
+.then(response => response.json())
 .then(data => {
     function filterMovies(range, movies) {
         return movies.filter(movie => {
